f0238393c8d6e1a2acd0f20a57f600cf023d72e1
Carlos Martínez <ced.mendoza@gmail.com>
2024-01-02 15:02:28 -0600

    setup: Add raylib vendor dependency

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include <raylib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    InitWindow(800, 800, "SimpleLandmassGeneration");
+    SetWindowMonitor(0);
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        {
+            ClearBackground(BLACK);
+            DrawText("Hello world", 100, 100, 28, WHITE);
+
+            DrawRectangle(0, 0, 80, 20, WHITE);
+            DrawFPS(0, 0);
+        }
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
