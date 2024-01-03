b138f5c3d985e440cfcfb0f380f34e5ee3343fa3
Carlos Martínez <ced.mendoza@gmail.com>
2024-01-02 23:48:01 -0600

    gui: Add offset x, y to options, automatically handle Y coordinate. map: Generate perlin noise with custom function which receives seed param. map: Use seed and offset_x,_y options when generating map

--- main.c
+++ main.c
@@ -8,17 +8,26 @@
 
 #define SEED_MIN 0
 #define SEED_MAX 1000000
+
 #define SCALE_MIN 1
 #define SCALE_DEF 10
 #define SCALE_MAX 1000
 #define SCALE_FACTOR 10.0f
 
+#define OFFSET_X_MIN (-100)
+#define OFFSET_X_DEF 0
+#define OFFSET_X_MAX 100
+
+#define OFFSET_Y_MIN (-100)
+#define OFFSET_Y_DEF 0
+#define OFFSET_Y_MAX 100
+
 #define WIDTH_MIN 100
-#define WIDTH_DEF 100
+#define WIDTH_DEF 500
 #define WIDTH_MAX 10000
 
 #define HEIGHT_MIN 100
-#define HEIGHT_DEF 100
+#define HEIGHT_DEF 500
 #define HEIGHT_MAX 10000
 
 #define OCTAVES_MIN 1
@@ -39,6 +48,12 @@ typedef struct {
     int seed;
     bool seed_edit_mode;
     bool clicked_random_seed;
+
+    int offset_x;
+    bool offset_x_edit_mode;
+
+    int offset_y;
+    bool offset_y_edit_mode;
 
     int width;
     bool width_edit_mode;
@@ -73,42 +88,54 @@ void DrawGUI(GeneratorOptions *options) {
     // Seed
     GuiSetStyle(VALUEBOX, TEXT_ALIGNMENT, TEXT_ALIGN_RIGHT);
     GuiSetStyle(SPINNER, TEXT_ALIGNMENT, TEXT_ALIGN_RIGHT);
-    if (GuiValueBox((Rectangle) { .x = 5, .y = 30, .width=155, .height = 20 }, " Seed", &options->seed, SEED_MIN, SEED_MAX, options->seed_edit_mode)) {
+    float layout_y = 5.0f;
+
+    if (GuiValueBox((Rectangle) { .x = 5, .y = (layout_y+=25), .width=155, .height = 20 }, " Seed", &options->seed, SEED_MIN, SEED_MAX, options->seed_edit_mode)) {
         options->seed_edit_mode = !options->seed_edit_mode;
     }
-    options->clicked_random_seed = GuiButton((Rectangle) { .x = 5, .y = 55, .width=190, .height = 20}, "Generate random");
+    options->clicked_random_seed = GuiButton((Rectangle) { .x = 5, .y = (layout_y+=25), .width=190, .height = 20}, "Generate random");
 
     // Scale
-    if (GuiSpinner((Rectangle) { .x = 5, .y = 80, .width=175, .height = 20 }, " S", &options->scale, SCALE_MIN, SCALE_MAX, options->scale_edit_mode)) {
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " S", &options->scale, SCALE_MIN, SCALE_MAX, options->scale_edit_mode)) {
         options->scale_edit_mode = !options->scale_edit_mode;
     }
 
+    // Offset X
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " X", &options->offset_x, OFFSET_X_MIN, OFFSET_X_MAX, options->offset_x_edit_mode)) {
+        options->offset_x_edit_mode = !options->offset_x_edit_mode;
+    }
+
+    // Offset Y
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " Y", &options->offset_y, OFFSET_Y_MIN, OFFSET_Y_MAX, options->offset_y_edit_mode)) {
+        options->offset_y_edit_mode = !options->offset_y_edit_mode;
+    }
+
     // Width
-    if (GuiSpinner((Rectangle) { .x = 5, .y = 105, .width=175, .height = 20 }, " W", &options->width, WIDTH_MIN, WIDTH_MAX, options->width_edit_mode)) {
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " W", &options->width, WIDTH_MIN, WIDTH_MAX, options->width_edit_mode)) {
         options->width_edit_mode = !options->width_edit_mode;
     }
 
     // Height
-    if (GuiSpinner((Rectangle) { .x = 5, .y = 130, .width=175, .height = 20 }, " H", &options->height, HEIGHT_MIN, HEIGHT_MAX, options->height_edit_mode)) {
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " H", &options->height, HEIGHT_MIN, HEIGHT_MAX, options->height_edit_mode)) {
         options->height_edit_mode = !options->height_edit_mode;
     }
 
     // Octaves
-    if (GuiSpinner((Rectangle) { .x = 5, .y = 155, .width=175, .height = 20 }, " O", &options->octaves, OCTAVES_MIN, OCTAVES_MAX, options->octaves_edit_mode)) {
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " O", &options->octaves, OCTAVES_MIN, OCTAVES_MAX, options->octaves_edit_mode)) {
         options->octaves_edit_mode = !options->octaves_edit_mode;
     }
 
     // Gain
-    if (GuiSpinner((Rectangle) { .x = 5, .y = 185, .width=175, .height = 20 }, " G", &options->gain, GAIN_MIN, GAIN_MAX, options->gain_edit_mode)) {
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " G", &options->gain, GAIN_MIN, GAIN_MAX, options->gain_edit_mode)) {
         options->gain_edit_mode = !options->gain_edit_mode;
     }
 
     // Lacunarity
-    if (GuiSpinner((Rectangle) { .x = 5, .y = 210, .width=175, .height = 20 }, " L", &options->lacunarity, LACUNARITY_MIN, LACUNARITY_MAX, options->lacunarity_edit_mode)) {
+    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " L", &options->lacunarity, LACUNARITY_MIN, LACUNARITY_MAX, options->lacunarity_edit_mode)) {
         options->lacunarity_edit_mode = !options->lacunarity_edit_mode;
     }
 
-    options->generate = GuiButton((Rectangle) { .x = 5, .y = 235, .width=190, .height = 20}, "Generate");
+    options->generate = GuiButton((Rectangle) { .x = 5, .y = (layout_y+=25), .width=190, .height = 20}, "Generate");
 }
 
 ProceduralMap *NewProceduralMap(Vector2 position) {
@@ -129,12 +156,28 @@ void UnloadProceduralMap(ProceduralMap *map) {
     MemFree(map);
 }
 
+float PerlinNoise2D(float x, float y, float lacunarity, float gain, int octaves, int seed) {
+    // This is basically a copy-paste from raylib, whoever it handles custom seed with octaves
+    int i;
+    float frequency = 1.0f;
+    float amplitude = 1.0f;
+    float sum = 0.0f;
+
+    for (i = 0; i < octaves; i++) {
+        sum += stb_perlin_noise3_seed(x*frequency,y*frequency,1.0f*frequency,0,0,0,(unsigned char)i+seed)*amplitude;
+        frequency *= lacunarity;
+        amplitude *= gain;
+    }
+    return sum;
+}
+
 void GenerateProceduralMap(ProceduralMap *map, GeneratorOptions options) {
-    // This is basically a copy-paste from `rtextures.c` file, whoever it handles custom options
+    // This is basically a copy-paste from raylib, whoever it handles custom options
     size_t total_size = options.width * options.height;
     Color *pixels = MemAlloc(sizeof(Color) * total_size);
-    int offset_x = 0;
-    int offset_y = 0; // TODO: Add offset to generator options
+    int offset_x = options.offset_x;
+    int offset_y = options.offset_y;
+    int seed = options.seed;
     int width = options.width;
     int height = options.height;
     float scale = (float)options.scale / SCALE_FACTOR;
@@ -146,8 +189,7 @@ void GenerateProceduralMap(ProceduralMap *map, GeneratorOptions options) {
         for (int x = 0;x < options.width; x++) {
             float nx = (float)(x + offset_x)*(scale/(float)width);
             float ny = (float)(y + offset_y)*(scale/(float)height);
-            // TODO: Make seed work with perlin noise
-            float p = stb_perlin_fbm_noise3(nx, ny, 1.0f, lacunarity, gain, octaves);
+            float p = PerlinNoise2D(nx, ny, lacunarity, gain, octaves, seed);
             p = Clamp(p, -1.0f, 1.0f);
 
             float np = (p + 1.0f)/2.0f;
@@ -193,6 +235,10 @@ int main() {
             .clicked_random_seed = false,
             .scale = SCALE_DEF,
             .scale_edit_mode = false,
+            .offset_x = OFFSET_X_DEF,
+            .offset_x_edit_mode = false,
+            .offset_y = OFFSET_Y_DEF,
+            .offset_y_edit_mode = false,
             .width = WIDTH_DEF,
             .width_edit_mode = false,
             .height = HEIGHT_DEF,
