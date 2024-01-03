c3b81585ae0feacbc99fe8ae15e1112e1ccea3a5
Carlos Martínez <ced.mendoza@gmail.com>
2024-01-03 00:33:42 -0600

    fix: Reformat code

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <raymath.h>
 
 #define RAYGUI_IMPLEMENTATION
+
 #include <raygui.h>
 #include <jungle/style_jungle.h>
 #include <stb_perlin.h>
@@ -44,7 +45,7 @@
 #define LACUNARITY_MAX 1000
 #define LACUNARITY_FACTOR 100.0f
 
-#define GENERATE_MAP_SECS 0.2f
+#define GENERATE_MAP_SECS 0.1f
 
 typedef struct {
     bool auto_generate;
@@ -88,62 +89,74 @@ typedef struct {
 } ProceduralMap;
 
 void DrawGUI(ProceduralMapOptions *options) {
-    GuiPanel((Rectangle) { .x = 0, .y = 0, .width=200, .height=720 }, "Options");
+    GuiPanel((Rectangle) {.x = 0, .y = 0, .width=200, .height=720}, "Options");
     float layout_y = 5.0f;
 
     GuiSetStyle(VALUEBOX, TEXT_ALIGNMENT, TEXT_ALIGN_RIGHT);
     GuiSetStyle(SPINNER, TEXT_ALIGNMENT, TEXT_ALIGN_RIGHT);
 
     // Preview mode
-    GuiToggleGroup((Rectangle) { .x = 5, .y = (layout_y+=25), .width=50, .height = 20 }, "HMAP;RMAP;MESH", &options->active_view);
+    GuiToggleGroup((Rectangle) {.x = 5, .y = (layout_y += 25), .width=50, .height = 20}, "HMAP;RMAP;MESH",
+                   &options->active_view);
 
     // Seed
-    if (GuiValueBox((Rectangle) { .x = 5, .y = (layout_y+=25), .width=155, .height = 20 }, " Seed", &options->seed, SEED_MIN, SEED_MAX, options->seed_edit_mode)) {
+    if (GuiValueBox((Rectangle) {.x = 5, .y = (layout_y += 25), .width=155, .height = 20}, " Seed", &options->seed,
+                    SEED_MIN, SEED_MAX, options->seed_edit_mode)) {
         options->seed_edit_mode = !options->seed_edit_mode;
     }
-    options->clicked_random_seed = GuiButton((Rectangle) { .x = 5, .y = (layout_y+=25), .width=190, .height = 20}, "Generate random");
+    options->clicked_random_seed = GuiButton((Rectangle) {.x = 5, .y = (layout_y += 25), .width=190, .height = 20},
+                                             "Generate random");
 
     // Scale
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " S", &options->scale, SCALE_MIN, SCALE_MAX, options->scale_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " S", &options->scale,
+                   SCALE_MIN, SCALE_MAX, options->scale_edit_mode)) {
         options->scale_edit_mode = !options->scale_edit_mode;
     }
 
     // Offset X
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " X", &options->offset_x, OFFSET_X_MIN, OFFSET_X_MAX, options->offset_x_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " X", &options->offset_x,
+                   OFFSET_X_MIN, OFFSET_X_MAX, options->offset_x_edit_mode)) {
         options->offset_x_edit_mode = !options->offset_x_edit_mode;
     }
 
     // Offset Y
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " Y", &options->offset_y, OFFSET_Y_MIN, OFFSET_Y_MAX, options->offset_y_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " Y", &options->offset_y,
+                   OFFSET_Y_MIN, OFFSET_Y_MAX, options->offset_y_edit_mode)) {
         options->offset_y_edit_mode = !options->offset_y_edit_mode;
     }
 
     // Width
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " W", &options->width, WIDTH_MIN, WIDTH_MAX, options->width_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " W", &options->width,
+                   WIDTH_MIN, WIDTH_MAX, options->width_edit_mode)) {
         options->width_edit_mode = !options->width_edit_mode;
     }
 
     // Height
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " H", &options->height, HEIGHT_MIN, HEIGHT_MAX, options->height_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " H", &options->height,
+                   HEIGHT_MIN, HEIGHT_MAX, options->height_edit_mode)) {
         options->height_edit_mode = !options->height_edit_mode;
     }
 
     // Octaves
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " O", &options->octaves, OCTAVES_MIN, OCTAVES_MAX, options->octaves_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " O", &options->octaves,
+                   OCTAVES_MIN, OCTAVES_MAX, options->octaves_edit_mode)) {
         options->octaves_edit_mode = !options->octaves_edit_mode;
     }
 
     // Gain
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " G", &options->gain, GAIN_MIN, GAIN_MAX, options->gain_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " G", &options->gain,
+                   GAIN_MIN, GAIN_MAX, options->gain_edit_mode)) {
         options->gain_edit_mode = !options->gain_edit_mode;
     }
 
     // Lacunarity
-    if (GuiSpinner((Rectangle) { .x = 5, .y = (layout_y+=25), .width=175, .height = 20 }, " L", &options->lacunarity, LACUNARITY_MIN, LACUNARITY_MAX, options->lacunarity_edit_mode)) {
+    if (GuiSpinner((Rectangle) {.x = 5, .y = (layout_y += 25), .width=175, .height = 20}, " L", &options->lacunarity,
+                   LACUNARITY_MIN, LACUNARITY_MAX, options->lacunarity_edit_mode)) {
         options->lacunarity_edit_mode = !options->lacunarity_edit_mode;
     }
 
-    options->manually_generate = GuiButton((Rectangle) { .x = 5, .y = (layout_y+=25), .width=190, .height = 20}, "Generate map");
+    options->manually_generate = GuiButton((Rectangle) {.x = 5, .y = (layout_y += 25), .width=190, .height = 20},
+                                           "Generate map");
 }
 
 ProceduralMap *NewProceduralMap(Vector2 position) {
@@ -172,7 +185,8 @@ float PerlinNoise2D(float x, float y, float lacunarity, float gain, int octaves,
     float sum = 0.0f;
 
     for (i = 0; i < octaves; i++) {
-        sum += stb_perlin_noise3_seed(x*frequency,y*frequency,1.0f*frequency,0,0,0,(unsigned char)i+seed)*amplitude;
+        sum += stb_perlin_noise3_seed(x * frequency, y * frequency, 1.0f * frequency, 0, 0, 0,
+                                      (unsigned char) i + seed) * amplitude;
         frequency *= lacunarity;
         amplitude *= gain;
     }
@@ -188,22 +202,22 @@ void GenerateHeightMap(ProceduralMap *map, ProceduralMapOptions options) {
     int seed = options.seed;
     int width = options.width;
     int height = options.height;
-    float scale = (float)options.scale / SCALE_FACTOR;
-    float lacunarity = (float)options.lacunarity / LACUNARITY_FACTOR;
-    float gain = (float)options.gain / GAIN_FACTOR;
+    float scale = (float) options.scale / SCALE_FACTOR;
+    float lacunarity = (float) options.lacunarity / LACUNARITY_FACTOR;
+    float gain = (float) options.gain / GAIN_FACTOR;
     int octaves = options.octaves;
 
     for (int y = 0; y < options.height; y++) {
-        for (int x = 0;x < options.width; x++) {
-            float nx = (float)(x + offset_x)*(scale/(float)width);
-            float ny = (float)(y + offset_y)*(scale/(float)height);
+        for (int x = 0; x < options.width; x++) {
+            float nx = (float) (x + offset_x) * (scale / (float) width);
+            float ny = (float) (y + offset_y) * (scale / (float) height);
             float p = PerlinNoise2D(nx, ny, lacunarity, gain, octaves, seed);
             p = Clamp(p, -1.0f, 1.0f);
 
-            float np = (p + 1.0f)/2.0f;
+            float np = (p + 1.0f) / 2.0f;
 
-            int intensity = (int)(np*255.0f);
-            pixels[y*width + x] = (Color) { intensity, intensity, intensity, 255 };
+            int intensity = (int) (np * 255.0f);
+            pixels[y * width + x] = (Color) {intensity, intensity, intensity, 255};
         }
     }
 
@@ -238,7 +252,7 @@ void GenerateProceduralMap(ProceduralMap *map, ProceduralMapOptions options) {
 
 void DrawProceduralMap(ProceduralMap *map, ProceduralMapOptions options) {
     if (options.active_view == 0 && IsTextureReady(map->noise_tex)) {
-        DrawTexture(map->noise_tex, (int)map->position.x, (int)map->position.y, WHITE);
+        DrawTexture(map->noise_tex, (int) map->position.x, (int) map->position.y, WHITE);
     }
 }
 
@@ -271,7 +285,7 @@ int main() {
             .lacunarity = LACUNARITY_DEF,
             .lacunarity_edit_mode = false,
     };
-    ProceduralMap *map = NewProceduralMap((Vector2) { 210, 10 });
+    ProceduralMap *map = NewProceduralMap((Vector2) {210, 10});
 
     float generate_map_timer = 0.0f;
     while (!WindowShouldClose()) {
@@ -285,22 +299,23 @@ int main() {
             options.auto_generate = false;
             DrawGUI(&options);
 
-            Rectangle drawing_area = { .x = 200, .y = 0, .width = (float)GetScreenWidth()-200, .height = (float)GetScreenHeight() };
+            Rectangle drawing_area = {.x = 200, .y = 0, .width = (float) GetScreenWidth() -
+                                                                 200, .height = (float) GetScreenHeight()};
             if (IsMouseButtonDown(MOUSE_MIDDLE_BUTTON) && CheckCollisionPointRec(GetMousePosition(), drawing_area)) {
-                options.offset_x -= (int)GetMouseDelta().x;
-                options.offset_y -= (int)GetMouseDelta().y;
+                options.offset_x -= (int) GetMouseDelta().x;
+                options.offset_y -= (int) GetMouseDelta().y;
                 options.auto_generate = true;
             }
 
             if (GetMouseWheelMove() != 0.0f && CheckCollisionPointRec(GetMousePosition(), drawing_area)) {
-                options.scale -= (int)(GetMouseWheelMove() * 5);
+                options.scale -= (int) (GetMouseWheelMove() * 5);
                 options.auto_generate = true;
             }
 
             // Handle input (from drawing/updating GUI):
             // Generate random value
             if (options.clicked_random_seed) {
-                options.seed = (int)GetRandomValue(SEED_MIN, SEED_MAX);
+                options.seed = (int) GetRandomValue(SEED_MIN, SEED_MAX);
                 options.auto_generate = true;
             }
 
